61bc498cc33d7783efb0d4c9ad071b775ac765c9
ramyagurram454129 <ramyagurram20@gmail.com>
2023-10-12 15:00:47 +0530

    committing the CountryAutocomplete

---
+++ Task13App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CountryAutocomplete from "./CountryAutocomplete";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Country Autocomplete Form</h1>
+      <CountryAutocomplete />
+    </div>
+  );
+}
+
+export default App;
