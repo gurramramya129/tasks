4fab0862f8273a0cc0ff640959a43dec2d699241
ramyagurram454129 <ramyagurram20@gmail.com>
2023-10-13 11:03:34 +0530

    committing the Navbar

---
+++ Task14App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Main Content</h1>
+    </div>
+  );
+}
+
+export default App;
